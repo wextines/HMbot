8869321860a9030bc3e8e297921603a01dc99400
wextines <otabek.wx@gmail.com>
2025-04-02 03:21:32 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from HMbot!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

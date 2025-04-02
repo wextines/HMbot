fda399a3b79f15c436a35cbce72d54317126bacd
wextin <wextines@gmail.com>
2025-04-03 01:04:43 +0500

    last version

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN='7846152188:AAGuff2EKF88CYgxdkacPXiDnUP7TsAhVbE'
-ADMIN_ID = '7999211529'
+ADMIN_ID = 7999211529
 DB_URL = 'postgresql://tgbot_w7dy_user:jukNLalM0f94mZVlStjnfKIWiQF7QKxg@dpg-cvm3fbvfte5s73ep6po0-a.oregon-postgres.render.com/tgbot_w7dy'

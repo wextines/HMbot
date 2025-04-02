f70ab4e59aca6d7de272ee7b1ca45d09295cc14a
wextines <otabek.wx@gmail.com>
2025-04-03 03:38:28 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN='7846152188:AAHR6Q42lxtUXmY25lr60NI67814I3Gvzv0'
-adminDB = 7999211529
+adminDB = 202814815
 DB_URL = 'postgresql://hmbot_porsgres_user:Pclz9OAPWdwFl2IBctuABsGtzB3MIPrR@dpg-cvmpp4nfte5s739b76p0-a.oregon-postgres.render.com/hmbot_porsgres'

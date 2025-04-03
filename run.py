1f4630b46119efe2ed6c0b12f807d3ff5b74c7a6
wextines <otabek.wx@gmail.com>
2025-04-03 11:03:31 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,18 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-
-
 from config import TOKEN
 from app.handler import router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-
-
 async def main():
     await bot.delete_webhook()
     dp.include_router(router)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot)  
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
+        asyncio.run(main())  
     except KeyboardInterrupt:
         print('Exit')
-
-
